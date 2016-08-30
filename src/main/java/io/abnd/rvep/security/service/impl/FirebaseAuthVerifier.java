81b7e13e237f0553bfc005525b2e5acc784bc2df
borysn <xborysn@gmail.com>
2016-08-30 00:08:16 -0500

    figuring out firebase tasks

--- src/main/java/io/abnd/rvep/security/service/impl/FirebaseAuthVerifier.java
+++ src/main/java/io/abnd/rvep/security/service/impl/FirebaseAuthVerifier.java
@@ -1,7 +1,12 @@
 package io.abnd.rvep.security.service.impl;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseToken;
+import com.google.firebase.tasks.Task;
 import io.abnd.rvep.security.model.intf.AuthToken;
 import io.abnd.rvep.security.service.intf.AuthVerifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -10,11 +15,14 @@ import java.security.GeneralSecurityException;
 @Service
 public class FirebaseAuthVerifier implements AuthVerifier {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     public boolean verify(AuthToken token) throws GeneralSecurityException, IOException {
-        //FirebaseConfig.getInstance().verifyIdToken(token.getTokenId());
-        return false;
-        // return
-        //return idToken != null;
+        Task<FirebaseToken> fbTask = FirebaseAuth.getInstance().verifyIdToken(token.getTokenId());
+
+        fbTask.getResult();
+
+        return fbTask.isSuccessful();
     }
 
 }
