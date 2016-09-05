5c2ab6953e722f7fcfd8783bfaa1911986138834
borysn <xborysn@gmail.com>
2016-09-05 06:43:08 -0500

    fix sql

--- data/db/scripts/init-db-tables.sql
+++ data/db/scripts/init-db-tables.sql
@@ -10,20 +10,20 @@
 use rvep;
 
 -- rvep_ categories
-insert into rvep_roll_category (id, name, enabled) value (1000, 'app', 1);
-insert into rvep_roll_category (id, name, enabled) value (1100, 'event', 1);
+insert into role_category (id, name, enabled) value (1000, 'app', 1);
+insert into role_category (id, name, enabled) value (1100, 'event', 1);
 
 -- app rvep_s
-insert into rvep_roll (name, enabled, rvep_category_id) values ('ROLE_USER', 1, 1000);
-insert into rvep_roll (name, enabled, rvep__category_id) values ('ROLE_ANONYMOUS', 1, 1000);
-insert into rvep_roll (name, enabled, rvep__category_id) values ('ROLE_ADMIN', 1, 1000);
+insert into rvep_role (name, enabled, role_category_id) values ('ROLE_USER', 1, 1000);
+insert into rvep_role (name, enabled, role_category_id) values ('ROLE_ANONYMOUS', 1, 1000);
+insert into rvep_role (name, enabled, role_category_id) values ('ROLE_ADMIN', 1, 1000);
 
 -- event rvep_s
-insert into rvep_roll (name, enabled, rvep_category_id) values ('ROLE_CREATOR', 1, 1100);
-insert into rvep_roll (name, enabled, rvep_category_id) values ('ROLE_PLANNER', 1, 1100);
-insert into rvep_roll (name, enabled, rvep_category_id) values ('ROLE_TASKER', 1, 1100);
-insert into rvep_roll (name, enabled, rvep_category_id) values ('ROLE_ATTENDE', 1, 1100);
-insert into rvep_roll (name, enabled, rvep_category_id) values ('ROLE_BYSTANDER', 1, 1100);
+insert into rvep_role (name, enabled, role_category_id) values ('ROLE_CREATOR', 1, 1100);
+insert into rvep_role (name, enabled, role_category_id) values ('ROLE_PLANNER', 1, 1100);
+insert into rvep_role (name, enabled, role_category_id) values ('ROLE_TASKER', 1, 1100);
+insert into rvep_role (name, enabled, role_category_id) values ('ROLE_ATTENDE', 1, 1100);
+insert into rvep_role (name, enabled, role_category_id) values ('ROLE_BYSTANDER', 1, 1100);
 
 -- auth providers
 insert into auth_provider (name, enabled) values ('google', 1);
