5f2073e4e1b22953d5b69fec7406a36d35c83de7
borysn <xborysn@gmail.com>
2016-09-05 18:28:59 -0500

    add provider

--- src/main/java/io/abnd/rvep/user/model/impl/RvepRegisterUserRequest.java
+++ src/main/java/io/abnd/rvep/user/model/impl/RvepRegisterUserRequest.java
@@ -4,10 +4,18 @@ import io.abnd.rvep.user.model.intf.RegisterUserRequest;
 
 public class RvepRegisterUserRequest implements RegisterUserRequest {
     private String username;
+    private String provider;
 
     @Override
     public void setUsername(String username) { this.username = username; }
+
+    @Override
+    public void setProvider(String provider) { this.provider = provider; }
+
     @Override
     public String getUsername() { return this.username; }
+
+    @Override
+    public String getProvider() { return this.provider; }
 
 }
--- src/main/java/io/abnd/rvep/user/model/intf/RegisterUserRequest.java
+++ src/main/java/io/abnd/rvep/user/model/intf/RegisterUserRequest.java
@@ -2,5 +2,7 @@ package io.abnd.rvep.user.model.intf;
 
 public interface RegisterUserRequest {
     void setUsername(String username);
+    void setProvider(String provider);
     String getUsername();
+    String getProvider();
 }
