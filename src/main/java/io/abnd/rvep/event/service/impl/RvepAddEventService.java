5072cf60425c52ea827ac0af03c8ba6fe6fee019
borysn <xborysn@gmail.com>
2016-09-08 12:05:26 -0500

    service annotation

--- src/main/java/io/abnd/rvep/event/service/impl/RvepAddEventService.java
+++ src/main/java/io/abnd/rvep/event/service/impl/RvepAddEventService.java
@@ -13,10 +13,12 @@ import io.abnd.rvep.user.dao.intf.RvepUserProfileDAO;
 import io.abnd.rvep.user.model.RvepUserProfile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Calendar;
 
+@Service
 public class RvepAddEventService implements AddEventService {
 
     private RvepEventDAO rvepEventDAO;
